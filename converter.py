bbd589c65075787f22c6e0df7b14f3d0f138e2ef
maxtres <67063568+Eraser53@users.noreply.github.com>
2023-06-01 15:48:31 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,23 +1,19 @@
 import re
 
-def convert_text(text):
-    text = text.lower()
-    text = re.sub(r'[^a-zA-Z0-9]', '', text)
-    return text
+def convert_text(file_path):
+    # Read the text from the file
+    with open(file_path, 'r') as file:
+        text = file.read()
 
-# Read input text from file
-file_path = "corrected_sentence.txt"  # Update with the path to your input file
+    # Convert the text to lowercase
+    lowercase_text = text.lower()
 
-with open(file_path, "r") as file:
-    input_text = file.read()
+    # Remove symbols using regular expressions
+    cleaned_text = re.sub(r'[^a-zA-Z0-9\s]', '', lowercase_text)
 
-# Convert the text
-converted_text = convert_text(input_text)
+    return cleaned_text
 
-# Write converted text to file
-output_file_path = "output.txt"  # Update with the path to your output file
-
-with open(output_file_path, "w") as file:
-    file.write(converted_text)
-
-print("Conversion complete. Converted text saved to:", output_file_path)
+# Example usage
+file_path = 'path/to/your/file.txt'  # Replace with the actual path to your text file
+converted_text = convert_text(file_path)
+print(converted_text)
