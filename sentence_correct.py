8a729f33038846485446a5ab9283b780c738ad40
maxtres <67063568+Eraser53@users.noreply.github.com>
2023-06-01 16:55:25 +0300

    Update sentence_correct.py

--- sentence_correct.py
+++ sentence_correct.py
@@ -1,25 +1,32 @@
-from textblob import Textblob
-import subprocess
+import os
 
-file1=open("input.txt", "r+")
-a=file1.read()
-
-print("You: ",str(a))
-
-b=TextBlob(a)
-
-file1.close()
-
-d=open("input.txt", "w")
-d.write(str(b.correct()))
-d.close()
-
-def run_another_file():
-    file_path = "converter.py"  # Replace with the actual path of the file you want to run
-
-    # Launch a new process to execute the file
-    subprocess.Popen(["python", file_path])
-
-# Main program
-if __name__ == "__main__":
-    run_another_file()
+def correct_sentences(input_file, output_file):
+    """
+    This function reads sentences from an input file, corrects them, and saves the corrected sentences to an output file.
+    
+    Parameters:
+    input_file (str): The name of the input file
+    output_file (str): The name of the output file
+    
+    Returns:
+    None
+    """
+    try:
+        # Check if input file exists
+        if not os.path.exists(input_file):
+            raise FileNotFoundError("Input file not found")
+        
+        # Open input and output files
+        with open(input_file, "r") as f_in, open(output_file, "w") as f_out:
+            # Read each line from input file
+            for line in f_in:
+                # Split line into sentences
+                sentences = line.strip().split(". ")
+                
+                # Correct each sentence and write to output file
+                for sentence in sentences:
+                    corrected_sentence = sentence.capitalize() + "."
+                    f_out.write(corrected_sentence + "\n")
+    except Exception as e:
+        # Log the error
+        print(f"Error: {e}")
