48e1db8cec5e9094ccb52c9a93b0bd7ce4d65115
maxtres <67063568+Eraser53@users.noreply.github.com>
2023-06-01 12:44:06 +0300

    Update sentence_correct.py

--- sentence_correct.py
+++ sentence_correct.py
@@ -1,34 +1,41 @@
-def create_biagrams(word): 
-  return [word[i] + word[i+1] for i in range(len(word)-1)]
+def create_bigrams(word):
+    return [word[i] + word[i+1] for i in range(len(word)-1)]
 
-def get_similarity_ratio(word1,word2):
-  word1, word2 = word1.lower(), word2.lower()
-  
-  common = []
-  
-  bigram1, bigram2 = create_bigram(word1), create_bigram(word2)
+def get_similarity_ratio(word1, word2):
+    word1, word2 = word1.lower(), word2.lower()
+    common = []
+    bigram1, bigram2 = create_bigrams(word1), create_bigrams(word2)
+    
+    for i in range(len(bigram1)):
+        try:
+            cmn_elt = bigram2.index(bigram1[i])
+            common.append(bigram1[i])
+        except ValueError:
+            continue
+    
+    return len(common) / max(len(bigram1), len(bigram2))
 
-  for i in range(len(bigram1)):
+def AutoCorrect(word, database={'etc'}, sim_threshold=0.5):
+    max_sim = 0.0
+    most_sim_word = word
 
-    # common elt
-try:
-      cmn_elt = bigram2.index(bigram1[i])
-      common.append(bigram1[i])
-      except:
-        continue
-        
-return len (common)/max(len(bigram1), len(bigram2))
-   
-def AutoCorrect(word, database={'etc'}), sim_threshold=0.5):
-  max_sim = 0.0
-  most_sim_word = word
-  
-  for data_word in database:
-    cur_sim = get_similarity_ratio(word, data_word)
-    if cur_sim > max_sim:
-      max_sim = cur_sim
-      max_sim_word = data_word
-      
-return most_sim_word if max_sim>sim_threshold else word
+    for data_word in database:
+        cur_sim = get_similarity_ratio(word, data_word)
+        if cur_sim > max_sim:
+            max_sim = cur_sim
+            most_sim_word = data_word
+
+    return most_sim_word if max_sim > sim_threshold else word
+
+# Example usage:
+input_word = input("Enter a word: ")
+corrected_word = AutoCorrect(input_word)
+print("Corrected word:", corrected_word)
+
+# Save the corrected word in a text file
+output_file = "corrected_word.txt"
+with open(output_file, 'w') as file:
+    file.write(corrected_word)
+print("Corrected word saved in", output_file)
 
       
