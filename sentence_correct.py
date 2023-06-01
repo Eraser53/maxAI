d9ca58d90db771bb87d1b13e09cd1cbbd379968a
maxtres <67063568+Eraser53@users.noreply.github.com>
2023-06-01 15:41:16 +0300

    Update sentence_correct.py

--- sentence_correct.py
+++ sentence_correct.py
@@ -1,36 +1,25 @@
-import difflib
+from textblob import Textblob
+import subprocess
 
-def create_bigrams(sentence):
-    return [sentence[i:i+2] for i in range(len(sentence)-1)]
+file1=open("input.txt", "r+")
+a=file1.read()
 
-def get_sentence_similarity(sentence1, sentence2):
-    sentence1 = sentence1.lower()
-    sentence2 = sentence2.lower()
-    bigrams1 = create_bigrams(sentence1)
-    bigrams2 = create_bigrams(sentence2)
-    matcher = difflib.SequenceMatcher(None, bigrams1, bigrams2)
-    return matcher.ratio()
+print("You: ",str(a))
 
-def auto_correct(sentence, database={'etc'}, sim_threshold=0.5):
-    max_sim = 0.0
-    most_sim_sentence = sentence
+b=TextBlob(a)
 
-    for data_sentence in database:
-        cur_sim = get_sentence_similarity(sentence, data_sentence)
-        if cur_sim > max_sim:
-            max_sim = cur_sim
-            most_sim_sentence = data_sentence
+file1.close()
 
-    return most_sim_sentence if max_sim > sim_threshold else sentence
+d=open("input.txt", "w")
+d.write(str(b.correct()))
+d.close()
 
-# Example usage:
-input_sentence = input("Enter a sentence: ")
-corrected_sentence = auto_correct(input_sentence)
-print("Corrected sentence:", corrected_sentence)
+def run_another_file():
+    file_path = "converter.py"  # Replace with the actual path of the file you want to run
 
-# Save the corrected sentence in a text file
-output_file = "corrected_sentence.txt"
-with open(output_file, 'w') as file:
-    file.write(corrected_sentence)
-print("Corrected sentence saved in", output_file)
+    # Launch a new process to execute the file
+    subprocess.Popen(["python", file_path])
 
+# Main program
+if __name__ == "__main__":
+    run_another_file()
