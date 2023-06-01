c5ec848ab82c07c1e1e655f0a012a7f88eecd57b
maxtres <67063568+Eraser53@users.noreply.github.com>
2023-06-01 15:52:28 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,9 +1,6 @@
 import re
 
 def convert_text(file_path):
-    # Read the text from the file
-    with open(file_path, 'r') as file:
-        text = file.read()
 # Read input text from file
 file_path = "corrected_sentence.txt"  # Update with the path to your input file
     # Convert the text to lowercase
@@ -18,6 +15,6 @@ file_path = "corrected_sentence.txt"  # Update with the path to your input file
     return cleaned_text
 
 # Example usage
-file_path = 'path/to/your/file.txt'  # Replace with the actual path to your text file
+file_path = 'file.txt'  # Replace with the actual path to your text file
 converted_text = convert_text(file_path)
 print(converted_text)
